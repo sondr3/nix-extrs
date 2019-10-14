26092a53593ddfe2dc64cf33b48e95458a1ddd6c
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-10-14 17:13:49 +0200

    Fetch NixOS package list

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+type Error = Box<dyn std::error::Error>;
+
+#[derive(Debug)]
+struct Config {
+    url: String,
+    package_path: String,
+    packages: Vec<String>,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Config {
+            url: "https://nixos.org/nixpkgs/".into(),
+            package_path: "packages-channels.json".into(),
+            packages: vec![],
+        }
+    }
+}
+
+impl Config {
+    fn get_packages(mut self) -> Self {
+        let resp: Vec<String> = reqwest::get(&format!("{}{}", self.url, self.package_path))
+            .expect("Could not fetch packages.")
+            .json()
+            .expect("Could not deserialize JSON response.");
+
+        self.packages = resp;
+        self
+    }
+}
+
+fn main() -> Result<(), Error> {
+    let config = Config::default().get_packages();
+
+    println!("{:?}", &config);
+
+    Ok(())
 }

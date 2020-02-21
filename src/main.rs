7703e0d26cf2029072cef88fc008b8ad166bd334
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-02-21 23:13:25 +0100

    Build index and create cache directory/files as needed

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use colored::*;
+use directories::ProjectDirs;
 use indicatif::{ProgressBar, ProgressStyle};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::Write;
+use std::path::PathBuf;
 use std::process::Command;
 use structopt::{clap::AppSettings, StructOpt};
 
@@ -15,11 +18,14 @@ type Error = Box<dyn std::error::Error>;
     global_settings = &[AppSettings::ColoredHelp, AppSettings::ArgRequiredElseHelp]
 )]
 struct CLI {
-    #[structopt(short, long)]
     /// Print verbose output.
+    #[structopt(short, long)]
     verbose: bool,
+    /// Update packages
+    #[structopt(short, long)]
+    update: bool,
     /// Package name to search for.
-    package: String,
+    package: Option<String>,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -62,8 +68,44 @@ struct Maintainer {
     name: String,
 }
 
-impl Packages {
-    fn build_index() {
+#[derive(Debug)]
+struct NixSearch {
+    cache_dir: PathBuf,
+    package_cache: PathBuf,
+}
+
+impl NixSearch {
+    fn new() -> Self {
+        let proj_dir = ProjectDirs::from("com", "sondr3", "nix-search")
+            .expect("Could not find project directory.");
+
+        let cache_dir: PathBuf = proj_dir.cache_dir().into();
+        let package_cache: PathBuf = [cache_dir.to_str().unwrap(), "pkgs.json"].iter().collect();
+
+        NixSearch {
+            cache_dir,
+            package_cache,
+        }
+    }
+
+    fn create(&self) {
+        self.create_cache().unwrap();
+        self.build_index();
+    }
+
+    fn cache_exists(&self) -> bool {
+        self.cache_dir.exists() && self.package_cache.exists()
+    }
+
+    fn create_cache(&self) -> std::io::Result<()> {
+        if !self.cache_exists() {
+            std::fs::create_dir(&self.cache_dir)?;
+        }
+
+        Ok(())
+    }
+
+    fn build_index(&self) {
         let spinner = ProgressBar::new_spinner();
         spinner.enable_steady_tick(80);
         spinner.set_style(
@@ -73,7 +115,7 @@ impl Packages {
         );
         spinner.set_message("Building package index...");
 
-        let mut file = File::create("pkgs.json").unwrap();
+        let mut file = File::create(&self.package_cache).unwrap();
         let command = Command::new("nix-env")
             .args(&["-f", "<nixpkgs>", "-qa", "--json"])
             .output()
@@ -90,9 +132,14 @@ impl Packages {
 
 fn main() -> Result<(), Error> {
     let cli = CLI::from_args();
-    println!("{:?}", cli);
 
-    Packages::build_index();
+    let search = NixSearch::new();
+    if cli.update {
+        search.build_index();
+    } else if !search.cache_exists() {
+        eprintln!("Cache directory missing, attempting to build index...");
+        search.create();
+    }
 
     Ok(())
 }

89a236792fa50cc5bd9fb616de9c5632d8c6adb9
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-02-22 01:05:36 +0100

    Optimize search by saving names into a list when building index

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader, Read, Write};
 use std::path::PathBuf;
-use std::process::Command;
+use std::process::{Command, Stdio};
 use structopt::{clap::AppSettings, StructOpt};
 
 type Error = Box<dyn std::error::Error>;
@@ -38,7 +38,6 @@ struct Package {
 
 #[derive(Debug, Deserialize, Serialize)]
 struct Meta {
-    available: Option<bool>,
     description: Option<String>,
     homepage: Option<Homepage>,
     // license: Vec<License>,
@@ -72,7 +71,9 @@ struct Maintainer {
 struct NixSearch {
     cache_dir: PathBuf,
     package_cache: PathBuf,
+    package_keys: PathBuf,
     packages: HashMap<String, Package>,
+    package_names: Vec<String>,
 }
 
 impl NixSearch {
@@ -82,17 +83,20 @@ impl NixSearch {
 
         let cache_dir: PathBuf = proj_dir.cache_dir().into();
         let package_cache: PathBuf = [cache_dir.to_str().unwrap(), "pkgs.json"].iter().collect();
+        let package_keys: PathBuf = [cache_dir.to_str().unwrap(), "keys.json"].iter().collect();
 
         NixSearch {
             cache_dir,
             package_cache,
+            package_keys,
             packages: HashMap::new(),
+            package_names: Vec::new(),
         }
     }
 
     fn create(&self) {
         self.create_cache().unwrap();
-        self.build_index();
+        self.build_index().unwrap();
     }
 
     fn cache_exists(&self) -> bool {
@@ -107,7 +111,8 @@ impl NixSearch {
         Ok(())
     }
 
-    fn build_index(&self) {
+    // TODO: Need to create an index of packages from `pname` value.
+    fn build_index(&self) -> std::io::Result<()> {
         let spinner = ProgressBar::new_spinner();
         spinner.enable_steady_tick(80);
         spinner.set_style(
@@ -117,29 +122,40 @@ impl NixSearch {
         );
         spinner.set_message("Building package index...");
 
-        let mut file = File::create(&self.package_cache).unwrap();
-        let command = Command::new("nix-env")
+        let mut command = Command::new("nix-env")
             .args(&["-f", "<nixpkgs>", "-qa", "--json"])
-            .output()
-            .unwrap();
+            .stdout(Stdio::piped())
+            .spawn()?;
 
-        if !&command.status.success() {
-            panic!("ohno");
+        if let Some(output) = command.stdout.take() {
+            let reader = BufReader::new(output);
+            let packages: HashMap<String, Package> = serde_json::from_reader(reader)?;
+            let pkgs: Vec<_> = packages.keys().collect();
+
+            serde_json::to_writer(File::create(&self.package_keys)?, &pkgs)?;
+            serde_json::to_writer(File::create(&self.package_cache)?, &packages)?;
         }
 
-        file.write_all(&command.stdout).unwrap();
-        spinner.finish_with_message("Updated!");
+        Ok(())
+
+        // file.write_all(&command.stdout).unwrap();
+        // spinner.finish_with_message("Updated!");
     }
 
     fn read_package_file(&mut self) {
-        let mut bytes = Vec::new();
-        File::open(&self.package_cache)
-            .unwrap()
-            .read_to_end(&mut bytes)
-            .unwrap();
-
-        self.packages = serde_json::from_slice(&bytes).unwrap();
+        self.packages = serde_json::from_slice(&read_file(&self.package_cache)).unwrap();
     }
+
+    fn read_key_file(&mut self) {
+        self.package_names = serde_json::from_slice(&read_file(&self.package_keys)).unwrap();
+    }
+}
+
+fn read_file(file: &PathBuf) -> Vec<u8> {
+    let mut bytes = Vec::new();
+    File::open(file).unwrap().read_to_end(&mut bytes).unwrap();
+
+    bytes
 }
 
 fn main() -> Result<(), Error> {
@@ -147,23 +163,26 @@ fn main() -> Result<(), Error> {
 
     let mut search = NixSearch::new();
     if cli.update {
-        search.build_index();
+        search.build_index()?;
     } else if !search.cache_exists() {
         eprintln!("Cache directory missing, attempting to build index...");
         search.create();
     }
 
+    search.read_key_file();
     search.read_package_file();
-    let name = &cli.package.unwrap();
-    let names = vec![name];
-    let mut pkgs: Vec<_> = search.packages.keys().collect();
-    pkgs.retain(|name| names.contains(name));
+    let name: String = cli.package.unwrap();
+    let pkgs: Vec<_> = search
+        .package_names
+        .iter()
+        .filter(|pkg| pkg.starts_with(&name))
+        .collect();
 
-    eprintln!("{:?}", pkgs);
+    eprintln!("{:#?}", pkgs);
 
     for pkg in pkgs {
         let pkg = search.packages.get(pkg);
-        println!("{:?}", pkg);
+        println!("{:#?}", pkg);
     }
 
     Ok(())
